8a53ac0ffddf01d5268cae1b194eff7a8e3b30d1
Artsy <craigspaeth@gmail.com>
2017-02-20 01:15:55 -0500

    Integrate with Circle's parallelizing features.

--- test/index.js
+++ test/index.js
@@ -1,67 +1,80 @@
-var exec = require('child_process').exec,
-    fs = require('fs');
+/* eslint-env mocha */
+const exec = require('child_process').exec
+const fs = require('fs')
 
-describe('ezel-assets', function() {
+describe('ezel-assets', function () {
+  beforeEach(function (done) {
+    exec('rm -rf public/assets', function (err) {
+      if (err) console.warn(err)
+      exec('mkdir -p public/assets', function (err) {
+        done(err)
+      })
+    })
+  })
 
-  beforeEach(function(done) {
-    exec('rm -rf public/assets', function(err) {
-      exec('mkdir -p public/assets', function(err) {
-        done(err);
-      });
-    });
-  });
-
-  it('generates CSS assets', function(done) {
+  it('generates CSS assets', function (done) {
     exec(
       'node bin/ezel-assets.js',
-      function(err, stdout, stderr) {
+      function (err, stdout, stderr) {
         fs.readFileSync(process.cwd() + '/public/assets/foo.css').toString()
-          .should.equal('h1{color:#00f}body{color:#f00}');
-        done(err);
+          .should.equal('h1{color:#00f}body{color:#f00}')
+        done(err)
       }
     )
-  });
+  })
 
-  it('generates JS assets', function(done) {
+  it('generates JS assets', function (done) {
     exec(
       'node bin/ezel-assets.js',
-      function(err, stdout, stderr) {
+      function (err, stdout, stderr) {
         fs.readFileSync(process.cwd() + '/public/assets/bar.js').toString()
-          .should.containEql('MODULE_NOT_FOUND');
-        done(err);
+          .should.containEql('MODULE_NOT_FOUND')
+        done(err)
       }
     )
-  });
+  })
 
-  it('generates gzipped assets', function(done) {
+  it('generates gzipped assets', function (done) {
     exec(
       'node bin/ezel-assets.js',
-      function(err, stdout, stderr) {
+      function (err, stdout, stderr) {
         fs.readFileSync(process.cwd() + '/public/assets/bar.js.jgz').toString()
-          .length.should.be.above(100);
-        done(err);
+          .length.should.be.above(100)
+        done(err)
       }
     )
-  });
+  })
 
-  it('uglifies the full bundle', function(done) {
+  it('uglifies the full bundle', function (done) {
     exec(
       'node bin/ezel-assets.js',
-      function(err, stdout, stderr) {
-        fs.readFileSync(process.cwd() + '/public/assets/foo.js').toString()
-        .should.not.containEql("\n");
-        done(err);
+      function (err, stdout, stderr) {
+        fs.readFileSync(process.cwd() + '/public/assets/bar.js').toString()
+          .should.not.containEql('\n')
+        done(err)
       }
     )
-  });
+  })
 
-  it('exits with 1 for a failed compile', function(done) {
+  it('exits with 1 for a failed compile', function (done) {
     exec(
       'node bin/ezel-assets.js fail/',
-      function(err, stderr) {
-        err.code.should.equal(1);
-        done();
+      function (err, stderr) {
+        err.code.should.equal(1)
+        done()
       }
     )
-  });
-});
+  })
+
+  it('integrates with Circle CI env variables', function (done) {
+    exec(
+      'CIRCLE_NODE_TOTAL=3 CIRCLE_NODE_INDEX=0 node bin/ezel-assets.js',
+      function (err, stdout, stderr) {
+        fs.readdirSync(process.cwd() + '/public/assets').toString()
+          .should.containEql('bar.js')
+          .should.not.containEql('foo.css')
+        done()
+      }
+    )
+  })
+})
